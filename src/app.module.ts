09b53374375d47b03cd57c1803250c70cc3b9bdb
jaytsol <jaytsol@naver.com>
2022-12-30 22:16:18 +0900

    fixed docker option & dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(
       //Replace this line with the one Cluster > Connect > Connect your Application
-      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.suflu.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.cwmntzi.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
     ),
   ],
   controllers: [AppController],
