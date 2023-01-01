df02cf33cc4c42b31d215332be99bd354128038d
jaytsol <jaytsol@naver.com>
2023-01-01 10:53:55 +0900

    added class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (error) => new BadRequestException(error),
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
